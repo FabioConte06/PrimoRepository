aa73d182ddbed1f8511c93ba9593589cd0954bdd
Fabio Contessotto <contessottofabio@itis-molinari.eu>
2024-10-16 13:50:33 +0200

    form completa

--- Albergo.js
+++ Albergo.js
@@ -6,7 +6,15 @@ const createForm = (parentElement) => {
         setlabels: (labels) => {data = labels},
         submit: (callbackinput) => {callback = callbackinput},
         render: () => {
-            
-        }
-    }
-}
+            parentElement.innerHTML = data.map((name) => {
+                return `<div class="form-group">${name}\n <input type="text" id="${name}" class="form-control"/></div>`;
+            }).join("\n") + `<button type="button" class="btn btn-primary" id="submit">SUBMIT</button>`;
+            document.querySelectorAll("#submit").onclick = () => {
+                const result = data.map((name) => {
+                    return document.querySelector("#" + name).value;
+                });
+                callback(result);
+            }
+            },
+        };
+    };
