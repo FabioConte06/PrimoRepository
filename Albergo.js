a0aea3de21af7fa6ec4537796ee3103e3fa7178c
Fabio Contessotto <contessottofabio@itis-molinari.eu>
2024-10-17 17:56:05 +0200

    form funzionante

--- Albergo.js
+++ Albergo.js
@@ -6,15 +6,14 @@ const createForm = (parentElement) => {
         setlabels: (labels) => {data = labels},
         submit: (callbackinput) => {callback = callbackinput},
         render: () => {
-            parentElement.innerHTML = data.map((date, singole, double, suite) => {
-                return `<div class="form-group">${date}\n <input type="text" id="${date}" class="form-control"/></div>`+
-                `<div class="form-group">${singole}\n <input type="text" id="${singole}" class="form-control"/></div>` +
-                `<div class="form-group">${double}\n <input type="text" id="${double}" class="form-control"/></div>` +
-                `<div class="form-group">${suite}\n <input type="text" id="${suite}" class="form-control"/></div>`;
+            parentElement.innerHTML = data.map((index) => {
+                return `<div class="form-group">
+                        ${index}\n <input type="date" id="${index}" class="form-control"/>
+                        </div>`;
             }).join("\n") + `<button type="button" class="btn btn-primary" id="submit">SUBMIT</button>`;
             document.querySelectorAll("#submit").onclick = () => {
-                const result = data.map((date, singole, double, suite) => {
-                    return document.querySelector("#" + date, singole, double, suite).value;
+                const result = data.map((index) => {
+                    return document.querySelector("#" + index).value;
                 });
                 callback(result);
             }
