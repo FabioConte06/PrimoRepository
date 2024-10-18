a66550c8493b07e31b7b4d29a1b387636b1161f5
FEDERICO <federico.saggiro@icloud.com>
2024-10-18 09:29:32 +0200

    table

--- progetto.js
+++ progetto.js
@@ -1,24 +1,27 @@
-const createTable = (parentElement) => {
-    let data;
+const createTable = (parentElement, data) => {
+    let htmlTable = "<table>";
+    htmlTable += data.map((row) => 
+        "<tr>" + row.map((col) => 
+            "<td>" + col + "</td>"
+            ).join("")
+        ).join("") + "</tr>";
+    htmlTable += "</table>";
+    parentElement.innerHTML = htmlTable;
+    let newrow;
     return {
-        build: (dataInput) => {
-            data = dataInput;
-        },
-        render: () => {
-            let htmlTable = `<table class="table">`;
-            htmlTable += data.map((row) => 
-                "<tr>" + row.map((col) => 
-                    "<td>" + col + "</td>"
-                ).join("")
-            ).join("") + "</tr>";
-
-            htmlTable += "</table>";
-            parentElement.innerHTML = htmlTable;
+        render: (listadata) => {
+            newrow.push(listadata);
+            let Row = "";
+            listadata.forEach((dato) => {
+                let htmlRow = "<tr>" + dato.map(d => `<td>${d}</td>`).join('') + "</tr>";
+                Row += htmlRow;
+            }) 
+            console.log(Row)
+            table.innerHTML = header + Row + "</tbody></table>";
         }
     }
 }
 
-const table = createTable(document.querySelector("#table"));
-table.build([["DATA", "SINGOLA", "DOPPIA", "SUITE"]]);
-table.render();
+const table = createTable(document.querySelector("#table"), ["DATA", "SINGOLA", "DOPPIA", "SUITE"]);
+table.render(["a", "b", "c", "d"]);
 
