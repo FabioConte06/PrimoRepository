8d8b84452a4e66f49d7e58bee5df855a9fa0f5de
Nicolas Baez <baeznicolas@itis-molinari.eu>
2024-10-20 13:09:56 +0200

    correzione form

--- Albergo.js
+++ Albergo.js
@@ -2,24 +2,24 @@ const createForm = (parentElement) => {
     let data;
     callback = null;
 
+    //da sistemare
+
     return {
         setlabels: (labels) => {data = labels},
         submit: (callbackinput) => {callback = callbackinput},
         render: () => {
             parentElement.innerHTML = data.map((index) => {
                 return `<div class="form-group">
-<<<<<<< HEAD
-                        ${index[0]}\n <input type="${index[1]}" id="#${index[0]}" class="form-control"/>
-=======
                         ${index[0]}\n <input type="${index[1]}" id="${index[0]}" class="form-control"/>
->>>>>>> 67e8650c0a933413b095d9fb545854be71669c73
                         </div>`;
             }).join("\n") + `<button type="button" class="btn btn-primary" id="submit">SUBMIT</button>`;
-            document.querySelector("#submit").onclick = () => {
+            document.getElementById("submit").onclick = () => {
+                console.log("ciao")
                 const result = data.map((index) => {
-                    return document.querySelector(`#${index[0]}`).value;
+                    return document.getElementById(index[0]).value;
                 });
-                console.log(result);
+                table.render(result)
+                callback(result);
             }
             },
         };
