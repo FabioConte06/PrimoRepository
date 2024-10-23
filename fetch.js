96ba023fbfd688b27a0c44ae38d3e05a74ba5f58
Nicolas Baez <baeznicolas@itis-molinari.eu>
2024-10-23 20:10:38 +0200

    fetch

--- fetch.js
+++ fetch.js
@@ -1,6 +1,8 @@
-const chiave = "cfca9d05-1553-449d-ae62-172a341239b5"
+const chiave = "cfca9d05-1553-449d-ae62-172a341239b5";
+let result_get=[];
 
 const SET = (chiave,value) => {
+   console.log(value)
     fetch("https://ws.progettimolinari.it/cache/set", {
         headers: {
            'Content-Type': 'application/json',
@@ -21,22 +23,31 @@ const SET = (chiave,value) => {
 }
 
 const GET = (chiave) => {
-    fetch("https://ws.progettimolinari.it/cache/get", {
-        headers: {
-           'Content-Type': 'application/json',
-           'key': chiave
-        },
-        method: "POST",
-        body: JSON.stringify({
-           key: "Hotel"
-        })
-     }).then(r => r.json())
-     .then(r => {
-        console.log(r.result);
-     })
-     .catch(r => {
-        console.log(r.result);
-     })
+   return new Promise((resolve,reject)=>{
+      try{
+      fetch("https://ws.progettimolinari.it/cache/get", {
+         headers: {
+            'Content-Type': 'application/json',
+            'key': chiave
+         },
+         method: "POST",
+         body: JSON.stringify({
+            key: "Hotel"
+         })
+      }).then(r => r.json())
+      .then(r => {
+         resolve(r.result);
+      })
+      }catch(error) {
+         reject(error)
+      }
+   })   
 }
-SET(chiave,[1,9,3,4])
-GET(chiave)
+const Aggiorna =(nuova_riga)=>{
+   GET(chiave).then(result_get => {
+      console.log(result_get);
+      result_get.push(nuova_riga)
+      console.log(result_get)
+      SET(chiave,result_get)
+   });
+}
